f0576aeff4d3bfa043ecb8c58edf9d6d13d26cb0
EliasAzami <90111737+EliasAzami@users.noreply.github.com>
2025-12-19 07:44:04 -0800

    Add pile and last_played properties to Game type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -27,6 +27,9 @@ export interface Game {
   max_players: number;
   current_turn: number | null;
   current_rank: string | null;
+  pile: string[];
+  last_played_count: number;
+  last_played_by: number | null
   created_at: Date;
   creator_username?: string;
   player_count?: number;
